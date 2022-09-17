c55ec368ed5e02c4d4603b7c6fcb60e1e7dbd56f
José Lucas <bezerra00714@hotmail.com>
2022-09-17 18:25:49 -0300

    projeto iniciado tailwind instalado e começo do carrossel estático

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,64 @@
-
-
+import "./styles/main.css";
+import logo from "./assets/logo.svg";
 function App() {
-  return
+  return(         //width maximo, margem eixoX auto, diplays flex, flex direction, alaign-items, margem eixoY
+    <div className="max-w-[1344] mx-auto flex flex-col items-center my-20">
+      <img src={logo} alt="" />
+              {/* tamanho da font 60px, color:white, peso da fonte 900  margem top 80px*/}
+      <h1 className="text-6xl text-white font-black mt-20">Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.</h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative">
+          <img src="./image-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">League of Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./image-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota II</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./image-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Counter Strike</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./image-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./image-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fotnite</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+        <a href="" className="relative">
+          <img src="./image-7.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">World of Warcraft</strong>
+            <span className="text-zinc-300 text-sm block mt-1">anuncios</span>
+          </div>
+        </a>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.tsx",
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      // para adicionar uma "classe" a mais no tailwind, pode fazer  isso aqui
+      backgroundImage: {
+        fundo: "url('/Fundo.png')",
+        "nlw-gradient": "linear-gradient(89.86deg, #9572FC 27%, #43E7AD 42%, #E1D55D 25%)",
+        "game-gradient": "linear-gradient(180deg, rgba(0,0,0,0) 0%, rgba(0,0,0,0.9) 67.08%)",
+      },
+    },
+  },
+  plugins: [],
+}
